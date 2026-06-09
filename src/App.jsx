39857d0c8c43a06e682826987754091c40d02672
Oluwole Adekunbi <emmanuelson202@gmail.com>
2026-06-09 01:44:53 +0100

    Initial Sejudam React project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() 
+{
+
+  return (
+    <div>
+      <h1 className="text-5xl text-center mt-20 font-bold">
+        Welcome to Sejudam
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
